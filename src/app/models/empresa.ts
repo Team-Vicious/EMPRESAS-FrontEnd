7d48e3276ded8fd58a418c41f9aefa31a2ebab8a
sebastianmauriz@gmail.com <sebastianmauriz@gmail.com>
2021-03-19 18:05:14 -0300

    creo los entity y las clase service

---
+++ src/app/models/empresa.ts
@@ -0,0 +1,10 @@
+export class Empresa {
+    id!: number;
+    denominacion!: string;
+    telefono !: string;
+    horarioAtencion!: string;
+    quienSomos !: string;
+    iframe !: string;
+    domicilio !: string;
+    email !: string;
+}
---
+++ src/app/models/noticia.ts
@@ -0,0 +1,12 @@
+export class Noticia {
+
+    id !: number; 
+titulo!: string;
+resumen !: string;
+imagen !: string;
+contenidoHtml !: string;
+publicada !: string;
+createAt!: Date;
+idEmpresa !: number;
+
+}
