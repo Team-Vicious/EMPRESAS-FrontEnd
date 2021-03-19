9930a9dfdc5b2c9c1e6e988b5e6e98cad926ee91
sebastianmauriz@gmail.com <sebastianmauriz@gmail.com>
2021-03-19 14:02:36 -0300

    creo todos los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { IndexComponent } from './components/index/index.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IndexComponent,
+    DetalleComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
